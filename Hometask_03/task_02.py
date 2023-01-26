90aa4d78d58bb224cb61625abd38e8e44a67f041
FrankHorrigun <FrankHorrigun@mail.ru>
2023-01-26 15:13:54 +0200

    3 домашка

--- Hometask_03/task_02.py
+++ Hometask_03/task_02.py
@@ -1,6 +1,26 @@
-#Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
-#Пример:
+# Пример:
 
-#- [2, 3, 4, 5, 6] => [12, 15, 16];
-#- [2, 3, 5, 6] => [12, 15]
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+list1 = [2, 3, 4, 5, 6]
+list2 = [2, 3, 5, 6]
+
+
+def Composition_of_elements(list):
+    composition = []
+    pivot = 0
+    if len(list) % 2 != 0:
+        pivot = int(len(list)/2+1)
+    else:
+        pivot = int(len(list)/2)
+
+    for i in range(pivot):
+        composition.append(list[i]*list[len(list)-i-1])
+    return composition
+
+
+print(Composition_of_elements(list1))
+print(Composition_of_elements(list2))
--- Hometask_03/task_05.py
+++ Hometask_03/task_05.py
@@ -1,5 +1,30 @@
-#Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 
-#Пример:
+# Пример:
 
-#- для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+# - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+n = int(input('Введите номер ряда Фибоначчи: '))
+
+
+fibonachi = [0, 1]
+neg_fibonachi = []
+
+# def fibonachi(n):
+#     if n in (1,2):
+#         return 1
+#     return fibonachi(n-1)+fibonachi(n-2)
+
+
+for i in range(2, n+1):
+    fibonachi.append(fibonachi[i-1]+fibonachi[i-2])
+
+for i in range(1, len(fibonachi)):
+    if i % 2 != 0:
+        neg_fibonachi.append(-fibonachi[len(fibonachi)-i])
+    else:
+        neg_fibonachi.append(fibonachi[len(fibonachi)-i])
+
+fibonachi_result = neg_fibonachi+fibonachi
+
+print(fibonachi_result)
